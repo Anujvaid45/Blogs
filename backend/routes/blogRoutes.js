b6c5ed7200f6384c3bc5aaa84bc3228e25143916
Anujvaid45 <vaidanuj80@gmail.com>
2023-12-09 22:42:02 +0530

    big changes

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -6,7 +6,7 @@ const BlogPost = require('../models/blogPost');
 // Get all blog posts
 router.get('/',requireSignIn, async (req, res) => {
   try {
-    const blogPosts = await BlogPost.find({ user_id: req.user.id });
+    const blogPosts = await BlogPost.find({ user_id: req.user._id });
     res.json(blogPosts);
   } catch (error) {
     res.json({ message: error.message });
@@ -18,7 +18,7 @@ router.post('/',requireSignIn, async (req, res) => {
   const blogPost = new BlogPost({
     title: req.body.title,
     content: req.body.content,
-    user_id: req.user.id,
+    user_id: req.user._id,
   });
 
   try {
