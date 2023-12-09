44a572513fd0703501517d56f3e728802374668d
Anujvaid45 <vaidanuj80@gmail.com>
2023-12-09 22:37:07 +0530

    big changes

--- backend/models/blogPost.js
+++ backend/models/blogPost.js
@@ -3,9 +3,13 @@ const mongoose = require('mongoose');
 const blogPostSchema = new mongoose.Schema({
   title: String,
   content: String,
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'users', // Make sure it matches the model name used in mongoose.model() for the User model
+  // user: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'users', // Make sure it matches the model name used in mongoose.model() for the User model
+  // },
+  user_id: {
+    type: String,
+    required: true,
   },
 });
 
