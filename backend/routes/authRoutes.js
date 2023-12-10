087b8ac53a3e10f52d41dfcdd3d0c75c31b39d9b
Anujvaid45 <vaidanuj80@gmail.com>
2023-12-10 12:29:20 +0530

    final changes in backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {requireSignIn,isAdmin} = require('../middleware/authMiddleware.js')
 //controller functions
-const {registerController,loginController,testController,forgotPasswordController, updateProfileController, getOrdersController, getAllOrdersController} = require('../controller/authController.js')
+const {registerController,loginController} = require('../controller/authController.js')
 
 //routing 
 //register
@@ -11,24 +11,7 @@ router.post('/register',registerController)
 //login
 router.post('/login',loginController)
 
-//forgot password
-router.post('/forgot-password',forgotPasswordController)
 
-//test routes
-router.get('/test',requireSignIn,isAdmin,testController)
-
-//protected user route auth
-router.get('/user-auth',requireSignIn,(req,res)=>{
-    res.status(200).send({ok:true});
-});
-
-//protected admin route auth
-router.get('/admin-auth',requireSignIn,isAdmin,(req,res)=>{
-    res.status(200).send({ok:true});
-});
-
-//update the user profile
-router.put('/profile',requireSignIn,updateProfileController)
 
 module.exports = router
 
